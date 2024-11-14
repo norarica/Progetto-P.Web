c55f025f9443c22195163e199e1395d08a6d284a
LukeGiacomelli <57633198+LukeGiacomelli@users.noreply.github.com>
2024-11-14 18:15:57 +0100

    Update pnl.sql

--- pnl.sql
+++ pnl.sql
@@ -1,47 +1,62 @@
-CREATE DATABASE PNL;
-USE PNL;
-
------TABELLA DEI PRODOTTI----
 CREATE TABLE PRODOTTI(
-    prodotto_id INT PRIMARY KEY,
-    categoria VARCHAR(200) NOT NULL,
-    prezzo DECIMAL(7,2) NOT NULL
+    Id_prod INT PRIMARY KEY,
+    Categoria VARCHAR(50) NOT NULL,
+    Descrizione VARCHAR(200) NOT NULL,
+    Dimensione INT,
+    Prezzo DECIMAL(7,2) NOT NULL,
+    Classe VARCHAR(10)
 );
 
------ TABELLA MODIFICA -------
 CREATE TABLE MODIFICA(
-    prodotto_id INT NOT NULL,
-    admin_cf VARCHAR(16) NOT NULL,
-    tipo VARCHAR(100),
-    datanascita_a DATE
-    PRIMARY KEY (prodotto_id, admin_cf),
+   	Id_prod INT NOT NULL,
+    Admin_cf VARCHAR(16) NOT NULL,
+    Tipo VARCHAR(100),
+    Data_modifica DATE,
+    PRIMARY KEY (Id_prod, Admin_cf),
     CONSTRAINT fk_prodotto
-        FOREIGN KEY (prodotto_id) REFERENCES PRODOTTI(prodotto_id)
+        FOREIGN KEY (Id_prod) REFERENCES PRODOTTI(Id_prod)
         ON DELETE CASCADE,
     CONSTRAINT fk_admin
-        FOREIGN KEY (admin_cf) REFERENCES ADMIN(admin_cf)
+        FOREIGN KEY (Admin_cf) REFERENCES ADMIN(Admin_cf)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Carrello(
+   	Id_prod INT NOT NULL,
+    Utente_cf VARCHAR(16) NOT NULL,
+    Quantità VARCHAR(100),
+    PRIMARY KEY (Id_prod, Utente_cf),
+    CONSTRAINT fk_prodotto
+        FOREIGN KEY (Id_prod) REFERENCES PRODOTTI(Id_prod)
         ON DELETE CASCADE,
+    CONSTRAINT fk_utente
+        FOREIGN KEY (Utente_cf) REFERENCES UTENTE(Utente_cf)
+        ON DELETE CASCADE
 );
 
-
------ TABELLA ADMIN -------
-CREATE TABLE MODIFICA(
-    admin_cf VARCHAR(16) PRIMARY KEY,
-    email_a VARCHAR(50) NOT NULL UNIQUE,
-    password_hash_a VARCHAR(255) NOT NULL,
-    nome_admin VARCHAR(100) NOT NULL,
-
+CREATE TABLE ADMIN(
+    Admin_cf VARCHAR(16) PRIMARY KEY,
+    Email_a VARCHAR(50) NOT NULL UNIQUE,
+    Password_hash_a VARCHAR(255) NOT NULL,
+    Nome_a VARCHAR(100) NOT NULL,
+    Cognome_a VARCHAR(100) NOT NULL
 );
 
---- TABELLA UTENTE ------
 CREATE TABLE UTENTE(
-    utente_cf VARCHAR(16) PRIMARY KEY,
-    email_u VARCHAR(50) NOT NULL UNIQUE,
-    password_hash_u VARCHAR(255) NOT NULL,
-    nome VARCHAR (32) NOT NULL,
-    cognome VARCHAR(32) NOT NULL,
-    foto_profilo VARCHAR(255), --- LA FOTO PROFILO VIENE SALVATA COME URL OVVERO COME INDIRIZZO IN MEMORIA O SERVER---
-    datanascita_u DATE NOT NULL,
-    interesse VARCHAR(128),
-    bloccato BOOLEAN, 
+    Utente_cf VARCHAR(16) PRIMARY KEY,
+    Email_u VARCHAR(50) NOT NULL UNIQUE,
+    Password_hash_u VARCHAR(255) NOT NULL,
+    Nome VARCHAR (32) NOT NULL,
+    Cognome VARCHAR(32) NOT NULL,
+    Datanascita_u DATE NOT NULL,
+    Interesse VARCHAR(128),
+    Bloccato BOOLEAN
+);
+
+CREATE TABLE UTENTI_EL(
+    Utente_cf VARCHAR(16) NOT NULL,
+    DataEl DATE NOT NULL,
+    Nome VARCHAR (32) NOT NULL,
+    Cognome VARCHAR(32) NOT NULL,
+    PRIMARY KEY(Utente_cf,DataEl)
 );
